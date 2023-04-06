ca10c251007cb120e66cb94c1bcd90dd78df8019
manhlamabc123 <tranducmanh12092001@gmail.com>
2023-04-06 10:40:20 +0700

    Fix bug in bash script

--- jit_train_jit_openstack.sh
+++ jit_train_jit_openstack.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 python jit_DExtended.py -train \
-    -train_data "data/openstack_train.pkl" \
+    -train_data "data/openstack_train_dextend.pkl" \
     -train_data_cc2ftr "data/openstack_train_cc2ftr.pkl" \
     -dictionary_data "data/openstack_dict.pkl"
