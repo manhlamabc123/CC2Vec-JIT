ceace7026b6465e7a395a2aa9aa1f4e201a5e236
manhlamabc123 <tranducmanh12092001@gmail.com>
2023-04-24 08:35:16 +0700

    Update bash script

--- jit_evaluate_jit_openstack.sh
+++ jit_evaluate_jit_openstack.sh
@@ -2,6 +2,6 @@
 
 python jit_DExtended.py -predict \
     -pred_data "data/openstack_test_dextend.pkl" \
-    -pred_data_cc2ftr "data/openstack_test_cc2ftr.pkl" \
+    -pred_data_cc2ftr "jit_extract_openstack.pkl" \
     -dictionary_data "data/openstack_dict.pkl" \
-    -load_model "model/openstack_djit_extend.pt"
+    -load_model "snapshot/2023-04-19_10-42-21/epoch_50.pt"
--- jit_extract_openstack.sh
+++ jit_extract_openstack.sh
@@ -3,5 +3,5 @@
 python jit_cc2ftr.py -predict \
     -predict_data "data/openstack_test.pkl" \
     -dictionary_data "data/openstack_dict.pkl" \
-    -load_model "snapshot/2023-04-14_11-18-17/epoch_50.pt" \
+    -load_model "snapshot/2023-04-19_18-30-20/epoch_50.pt" \
     -name "jit_extract_openstack.pkl"
