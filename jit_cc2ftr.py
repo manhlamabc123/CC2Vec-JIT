109dfcfb0e9c68fe485c9429edb53f60ae8eb0b3
manhlamabc123 <tranducmanh12092001@gmail.com>
2023-05-31 14:34:47 +0700

    Create preprocess module

--- jit_cc2ftr.py
+++ jit_cc2ftr.py
@@ -1,9 +1,8 @@
 import argparse
-import pickle
-import numpy as np 
-from jit_padding import padding_message, clean_and_reformat_code, padding_commit_code, mapping_dict_msg, mapping_dict_code, convert_msg_to_label
 from jit_cc2ftr_train import train_model
 from jit_cc2ftr_extracted import extracted_cc2ftr
+from jit_cc2ftr_preprocess import preprocess_data
+import torch
 
 def read_args():
     parser = argparse.ArgumentParser()
@@ -35,7 +34,7 @@ def read_args():
     parser.add_argument('--predict', action='store_true', help='predicting testing data')
 
     # Number of parameters for Attention model
-    parser.add_argument('-embed_size', type=int, default=64, help='the dimension of embedding vector')
+    parser.add_argument('-embed_size', type=int, default=768, help='the dimension of embedding vector')
     parser.add_argument('-hidden_size', type=int, default=32, help='the number of nodes in hidden layers')
     parser.add_argument('-dropout_keep_prob', type=float, default=0.5, help='dropout for training PatchNet')
     parser.add_argument('-l2_reg_lambda', type=float, default=1e-5, help='regularization rate')
@@ -51,56 +50,29 @@ def read_args():
     return parser
 
 if __name__ == '__main__':
-    params = read_args().parse_args()    
+    params = read_args().parse_args()
+    params.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    import torch._dynamo as dynamo
+    torch._dynamo.config.suppress_errors = True
+    torch.backends.cudnn.benchmark = True
     
     if params.train is True:
-        train_data = pickle.load(open(params.train_data, 'rb'))
-        train_ids, train_labels, train_messages, train_codes = train_data    
 
-        ids = train_ids
-        labels = list(train_labels)
-        msgs = train_messages
-        codes = train_codes
-        
-        dictionary = pickle.load(open(params.dictionary_data, 'rb'))
-        dict_msg, dict_code = dictionary  
+        data = preprocess_data(params)
 
-        pad_msg = padding_message(data=msgs, max_length=params.msg_length)
-        added_code, removed_code = clean_and_reformat_code(codes)
-        pad_added_code = padding_commit_code(data=added_code, max_file=params.code_file, max_line=params.code_line, max_length=params.code_length)
-        pad_removed_code = padding_commit_code(data=removed_code, max_file=params.code_file, max_line=params.code_line, max_length=params.code_length)
-
-        pad_msg = mapping_dict_msg(pad_msg=pad_msg, dict_msg=dict_msg)
-        pad_added_code = mapping_dict_code(pad_code=pad_added_code, dict_code=dict_code)
-        pad_removed_code = mapping_dict_code(pad_code=pad_removed_code, dict_code=dict_code)
-        pad_msg_labels = convert_msg_to_label(pad_msg=pad_msg, dict_msg=dict_msg)
-
-        data = (pad_added_code, pad_removed_code, pad_msg_labels, dict_msg, dict_code)   
         train_model(data=data, params=params)
+
         print('--------------------------------------------------------------------------------')
         print('--------------------------Finish the training process---------------------------')
         print('--------------------------------------------------------------------------------')
         exit()
     
     elif params.predict is True:
-        data = pickle.load(open(params.predict_data, 'rb'))
-        ids, labels, msgs, codes = data 
 
-        dictionary = pickle.load(open(params.dictionary_data, 'rb'))   
-        dict_msg, dict_code = dictionary  
-
-        pad_msg = padding_message(data=msgs, max_length=params.msg_length)
-        added_code, removed_code = clean_and_reformat_code(codes)
-        pad_added_code = padding_commit_code(data=added_code, max_file=params.code_file, max_line=params.code_line, max_length=params.code_length)
-        pad_removed_code = padding_commit_code(data=removed_code, max_file=params.code_file, max_line=params.code_line, max_length=params.code_length)
-
-        pad_msg = mapping_dict_msg(pad_msg=pad_msg, dict_msg=dict_msg)
-        pad_added_code = mapping_dict_code(pad_code=pad_added_code, dict_code=dict_code)
-        pad_removed_code = mapping_dict_code(pad_code=pad_removed_code, dict_code=dict_code)
-        pad_msg_labels = convert_msg_to_label(pad_msg=pad_msg, dict_msg=dict_msg)
-        
-        data = (pad_added_code, pad_removed_code, pad_msg_labels, dict_msg, dict_code)
         params.batch_size = 1
+
+        data = preprocess_data(params)
+        
         extracted_cc2ftr(data=data, params=params)
         print('--------------------------------------------------------------------------------')
         print('--------------------------Finish the extracting process-------------------------')
