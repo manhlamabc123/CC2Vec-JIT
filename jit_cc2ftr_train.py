7c1d51dc643284008bd12706b006d1a85217fda8
Jirachiii <trangdy1112@gmail.com>
2023-07-12 11:00:12 +0700

    Remove torch.compile

--- jit_cc2ftr_train.py
+++ jit_cc2ftr_train.py
@@ -20,7 +20,7 @@ def train_model(data, params):
 
     # Create model, optimizer, criterion
     model = HierachicalRNN(args=params).to(params.device)
-    model = torch.compile(model, backend="inductor")
+    # model = torch.compile(model, backend="inductor")
     optimizer = torch.optim.Adam(model.parameters(), lr=params.l2_reg_lambda)
     criterion = nn.BCEWithLogitsLoss()
     
