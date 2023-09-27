9c9e0196ca7ddef08f9e0334de3d6ccf9913d0da
AnhLD2610 <leducanh26102002@gmail.com>
2023-09-27 14:48:15 +0700

    Update gitignore

--- run_all.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-echo "Project: qt"
-bash run_cc2vec.sh qt
--- run_cc2vec.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-bash cc2vec_train.sh $1
-
-bash cc2vec_eval.sh $1
-
-bash deepjit_train.sh $1
-
-bash deepjit_eval.sh $1
