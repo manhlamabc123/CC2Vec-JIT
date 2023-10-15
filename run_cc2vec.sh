e6ec6534729c74bc71493532f93acde9eb993914
manhlamabc123 <tranducmanh12092001@gmail.com>
2023-10-15 22:56:25 +0700

    Turn data_dir into args

--- run_cc2vec.sh
+++ run_cc2vec.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-DATA_DIR="/home/manh/Documents/Data/splited-tan-dataset"
+# DATA_DIR="/home/manh/Documents/Data/splited-tan-dataset"
 EPOCHS=3
 echo "Project: $1"
 echo "Train: $2"
 echo "Test: $3"
 
-bash cc2vec_train.sh $1 $DATA_DIR $EPOCHS $2
+bash cc2vec_train.sh $1 $4 $EPOCHS $2
 
-bash cc2vec_eval.sh $1 $DATA_DIR $EPOCHS $2
+bash cc2vec_eval.sh $1 $4 $EPOCHS $2
 
-bash deepjit_train.sh $1 $DATA_DIR $EPOCHS $2
+bash deepjit_train.sh $1 $4 $EPOCHS $2
  
-bash deepjit_eval.sh $1 $DATA_DIR $EPOCHS $2 $3
+bash deepjit_eval.sh $1 $4 $EPOCHS $2 $3
