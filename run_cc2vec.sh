0b00353c8087bb80ebf82b85d9a933965c06d27a
manhlamabc123 <tranducmanh12092001@gmail.com>
2023-10-16 12:23:39 +0700

    Update bash script

--- run_cc2vec.sh
+++ run_cc2vec.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 
 # DATA_DIR="/home/manh/Documents/Data/splited-tan-dataset"
-EPOCHS=3
 echo "Project: $1"
 echo "Train: $2"
 echo "Test: $3"
+echo "Epochs: $5"
+echo "Data dir: $4"
 
-bash cc2vec_train.sh $1 $4 $EPOCHS $2
+bash cc2vec_train.sh $1 $4 $5 $2
 
-bash cc2vec_eval.sh $1 $4 $EPOCHS $2
+bash cc2vec_eval.sh $1 $4 $5 $2
 
-bash deepjit_train.sh $1 $4 $EPOCHS $2
+bash deepjit_train.sh $1 $4 $5 $2
  
-bash deepjit_eval.sh $1 $4 $EPOCHS $2 $3
+bash deepjit_eval.sh $1 $4 $5 $2 $3
