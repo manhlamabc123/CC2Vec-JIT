16520af94d626c88126d90b7d0d86cdf7c1a5397
manhlamabc123 <tranducmanh12092001@gmail.com>
2023-10-17 15:30:30 +0700

    Update scripts

--- run_all.sh
+++ run_all.sh
@@ -1,3 +1,17 @@
 #! /bin/bash
 
+bash run_cc2vec.sh cpython part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 50
+
+bash run_cc2vec.sh salt part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 50
+
 bash run_cc2vec.sh swift part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 50
+
+bash run_cc2vec.sh yuzu part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 50
+
+bash run_cc2vec.sh elasticsearch part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 50
+
+bash run_cc2vec.sh php-src part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 50
+
+bash run_cc2vec.sh shardingsphere part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 50
+
+bash run_cc2vec.sh tensorflow part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 50
