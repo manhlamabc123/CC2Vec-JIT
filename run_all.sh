e18a9c4eb0e40deb21b4750e5af300c0a687fed3
manhlamabc123 <tranducmanh12092001@gmail.com>
2023-10-20 10:00:43 +0700

    Update scripts

--- run_all.sh
+++ run_all.sh
@@ -6,10 +6,3 @@ bash run_cc2vec.sh flink part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE
 bash run_cc2vec.sh swift part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 50
 bash run_cc2vec.sh cpython part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 50
 bash run_cc2vec.sh salt part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 50
-bash run_cc2vec.sh pandas part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 50
-bash run_cc2vec.sh presto part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 50
-bash run_cc2vec.sh scipy part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 50
-bash run_cc2vec.sh scikit-learn part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 50
-bash run_cc2vec.sh Ghost part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 50
-bash run_cc2vec.sh tensorflow part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 50
-bash run_cc2vec.sh numpy part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 50
---
+++ run_all_2.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+bash run_cc2vec.sh pandas part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 50
+bash run_cc2vec.sh presto part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 50
+bash run_cc2vec.sh scipy part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 50
+bash run_cc2vec.sh scikit-learn part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 50
+bash run_cc2vec.sh Ghost part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 50
+bash run_cc2vec.sh tensorflow part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 50
+bash run_cc2vec.sh numpy part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/data/splited-tandataset 50
