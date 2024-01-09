570d914e5c5790a3e892ccd82b3d5a66c78d44fb
manhtdd <manh.td120901@gmail.com>
2024-01-09 11:45:01 +0700

    Update scripts

--- deepjit_eval.sh
+++ deepjit_eval.sh
@@ -2,14 +2,14 @@
 
 python jit_cc2ftr.py -predict \
     -project "$1_$4" \
-    -predict_data "$2/$1/commits/$1_$5.pkl" \
+    -predict_data "$2/$1/commits/deepjit_$1_$5.pkl" \
     -dictionary_data "$2/$1/commits/$1_$4_dict.pkl" \
     -load_model "snapshot/$1_$4/epoch_$3.pt" \
     -name "extracted_features_$1_$5.pkl"
 
 python jit_DExtended.py -predict \
     -project "$1_$4" \
-    -pred_data "$2/$1/commits/$1_$5_dextend.pkl" \
+    -pred_data "$2/$1/commits/deepjit_$1_$5_dextend.pkl" \
     -pred_data_cc2ftr "extracted_features_$1_$5.pkl" \
     -dictionary_data "$2/$1/commits/$1_$4_dict.pkl" \
     -load_model "snapshot_dextend/$1_$4/epoch_$3.pt"
