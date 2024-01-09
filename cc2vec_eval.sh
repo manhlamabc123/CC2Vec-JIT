299fa51aca5ec0bfec43c1cbe1cefc36c047e76c
manhtdd <manh.td120901@gmail.com>
2024-01-09 11:47:12 +0700

    Update scripts

--- cc2vec_eval.sh
+++ cc2vec_eval.sh
@@ -2,7 +2,7 @@
 
 python jit_cc2ftr.py -predict \
     -project "$1_$4" \
-    -predict_data "$2/$1/commits/cc2vec_$1_$4.pkl" \
+    -predict_data "$2/$1/commits/cc2vec_$1_$4_train.pkl" \
     -dictionary_data "$2/$1/commits/$1_$4_dict.pkl" \
     -load_model "snapshot/$1_$4/epoch_$3.pt" \
     -name "extracted_features_$1_$4.pkl"
