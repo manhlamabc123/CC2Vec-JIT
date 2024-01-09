1781d5fce10b537f119c3f12ffad22499571a4eb
manhtdd <manh.td120901@gmail.com>
2024-01-09 11:54:09 +0700

    Update scripts

--- cc2vec_train.sh
+++ cc2vec_train.sh
@@ -2,6 +2,6 @@
 
 python jit_cc2ftr.py -train \
     -project "$1_$4" \
-    -train_data "$2/$1/commits/$1_$4.pkl" \
-    -dictionary_data "$2/$1/commits/$1_$4_dict.pkl" \
+    -train_data "$2/$1/commits/cc2vec_$1_$4_train.pkl" \
+    -dictionary_data "$2/$1/commits/$1_$4_train_dict.pkl" \
     -num_epochs $3
