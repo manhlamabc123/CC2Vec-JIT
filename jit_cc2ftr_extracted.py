9677c92351c9c53dcc8b748fa9148bb9b20b0889
manhtdd <manh.td120901@gmail.com>
2024-01-10 12:57:33 +0700

    Debug

--- jit_cc2ftr_extracted.py
+++ jit_cc2ftr_extracted.py
@@ -24,8 +24,9 @@ def extracted_cc2ftr(data, params):
 
             commit_ftr = model.forward_commit_embeds_diff(pad_added_code, pad_removed_code, state_hunk, state_sent, state_word)
 
+            print(commit_ftr)
+
             commit_ftrs.append(commit_ftr)
             
         commit_ftrs = torch.cat(commit_ftrs).cpu().detach().numpy()
-        print(commit_ftrs.size())
     pickle.dump(commit_ftrs, open(params.name, 'wb'))
