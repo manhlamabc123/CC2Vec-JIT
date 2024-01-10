9646a968f56aa59bff326eaec61319bc9e68a78c
manhtdd <manh.td120901@gmail.com>
2024-01-10 13:05:04 +0700

    Remove debug

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-bash run_cc2vec.sh rocksdb part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/crawling/truncated_median_date_tan_dataset 1
+bash run_cc2vec.sh rocksdb part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/crawling/truncated_median_date_tan_dataset 50
 bash run_cc2vec.sh php-src part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/crawling/truncated_tan_dataset 50
 bash run_cc2vec.sh flink part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/crawling/truncated_tan_dataset 50
 bash run_cc2vec.sh swift part_1_part_4 part_5 /data/gpfs/projects/punim1928/RISE/JITDP/crawling/truncated_tan_dataset 50
--- run_cc2vec.sh
+++ run_cc2vec.sh
@@ -11,6 +11,6 @@ bash cc2vec_train.sh $1 $4 $5 $2
 
 bash cc2vec_eval.sh $1 $4 $5 $2
 
-# bash deepjit_train.sh $1 $4 $5 $2
+bash deepjit_train.sh $1 $4 $5 $2
  
-# bash deepjit_eval.sh $1 $4 $5 $2 $3
+bash deepjit_eval.sh $1 $4 $5 $2 $3
