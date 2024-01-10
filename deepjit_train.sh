a35555cd748e694da059ded64ffea609c827e73f
manhtdd <manh.td120901@gmail.com>
2024-01-10 09:06:36 +0700

    Update scripts

--- deepjit_train.sh
+++ deepjit_train.sh
@@ -2,7 +2,7 @@
 
 python jit_DExtended.py -train \
     -project "$1_$4" \
-    -train_data "$2/$1/commits/deepjitr_$1_$4_train.pkl" \
+    -train_data "$2/$1/commits/deepjit_$1_$4_train.pkl" \
     -train_data_cc2ftr "extracted_features_$1_$4.pkl" \
     -dictionary_data "$2/$1/commits/$1_$4_train_dict.pkl" \
     -num_epochs $3
