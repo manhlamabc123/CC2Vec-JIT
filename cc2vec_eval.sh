8d321fba78d8ae5c479d7b3b59813dc6e0579af1
manhtdd <manh.td120901@gmail.com>
2024-01-10 13:01:54 +0700

    Debug

--- cc2vec_eval.sh
+++ cc2vec_eval.sh
@@ -5,3 +5,4 @@ python jit_cc2ftr.py -predict \
     -predict_data "$2/$1/commits/cc2vec_$1_$4_train.pkl" \
     -dictionary_data "$2/$1/commits/$1_$4_train_dict.pkl" \
     -load_model "snapshot/$1_$4/epoch_$3.pt" \
+    -name "extracted_features_$1_$5.pkl"
